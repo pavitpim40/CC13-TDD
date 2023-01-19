0a0567ee26be71af71d093c52adec98ef7f85fd8
Pavit Pimchanagul <pavitpim@gmail.com>
2023-01-19 20:20:25 +0700

    remove comment

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -3,25 +3,15 @@ const ValidationException = require('../errors/ValidationException');
 const UserService = require('../services/UserServices');
 
 exports.register = async (req, res, next) => {
-  // if (Object.keys(req.validationErrors).length > 0) {
-  //   return res.status(400).send({ validationErrors: req.validationErrors });
-  // }
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    // const validationErrors = {};
-    // errors.array().forEach((error) => {
-    //   validationErrors[error.param] = req.t(error.msg);
-    // });
-    // return res.status(400).send({ validationErrors });
     return next(new ValidationException(errors));
   }
   try {
     await UserService.createAccount(req.body);
     return res.status(200).send({ message: req.t('user_create_success') });
   } catch (error) {
-    // return res.status(502).send({ message: req.t(error.message) });
     next(error);
-    // return res.status(500);
   }
 };
 
@@ -32,7 +22,6 @@ exports.activateAccount = async (req, res, next) => {
     res.send({ message: req.t('account_activation_success') });
     res.send();
   } catch (error) {
-    // res.status(400).send({ message: req.t(error.message) });
     next(error);
   }
 };
